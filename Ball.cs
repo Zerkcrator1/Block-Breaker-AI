a72334c110fd5af2f39ef8c350213dd889733b6f
Zerkcrator1 <107038280+Zerkcrator1@users.noreply.github.com>
2022-09-25 19:16:07 -0500

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float ballForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce (new Vector2 (ballForce, ballForce));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Paddle.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float maxX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis ("Horizontal");
+
+        if (x < 0)
+        {
+            MoveLeft();
+        }
+        else if(x > 0)
+        {
+            MoveRight();
+        }
+        else
+        {
+            Stop();
+        }
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x,-maxX,maxX);
+        transform.position = pos;
+    }
+    void MoveLeft()
+    {
+        rb.velocity = new Vector2 (-speed,0);
+    }
+    void MoveRight()
+    {
+        rb.velocity = new Vector2 (speed,0);
+    }
+
+    void Stop()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
